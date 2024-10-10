1e63c95072617d535b05401b6d32d01a1883e707
Songonha <105191398+songonha@users.noreply.github.com>
2024-10-10 09:02:55 +0700

    Update CCIPTokenAndDataReceiver.sol

--- contracts/CCIPTokenAndDataReceiver.sol
+++ contracts/CCIPTokenAndDataReceiver.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+
+contract MyNFT is ERC721URIStorage, OwnerIsCreator {
+    string constant TOKEN_URI =
+        "https://ipfs.io/ipfs/QmYuKY45Aq87LeL1R5dhb1hqHLp6ZFbJaCP8jxqKM1MX6y/babe_ruth_1.json";
+    uint256 internal tokenId;
+
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    function mint(address to) public onlyOwner {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, TOKEN_URI);
+        unchecked {
+            tokenId++;
+        }
+    }
+}
